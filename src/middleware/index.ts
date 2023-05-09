47d3dbf3ce9625dd52834c0ad3d95264c221a912
luisdev97 <amadormontorodavid@gmail.com>
2023-05-09 21:51:26 +0100

    feat: search clients routed protected by roles

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,2 +1,4 @@
 export * from "./error-handler";
 export * from "./validation";
+export * from "./checkJwt";
+export * from "./checkRole";
--- src/routes/v1/clients.ts
+++ src/routes/v1/clients.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 
 import { getClientById, getClientByName } from "../../controllers";
+import { checkJwt, checkRole } from "../../middleware";
 
 const router = Router();
+const isTestEnvironment = process.env.NODE_ENV == "test";
 
-router.get("/:id", getClientById);
-router.get("/", getClientByName);
+router.get(
+  "/:id",
+  isTestEnvironment ? [] : [checkJwt, checkRole(["user", "admin"], true)],
+  getClientById
+);
+router.get(
+  "/",
+  isTestEnvironment ? [] : [checkJwt, checkRole(["user", "admin"], true)],
+  getClientByName
+);
 
 export default router;
