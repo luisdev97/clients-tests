da3401ff98f3e67bb0f80059ed9fd42549cc2dc0
luisdev97 <amadormontorodavid@gmail.com>
2023-05-09 21:39:57 +0100

    test: not found email in login  use case tested

--- src/controllers/auth/__tests__/login.test.ts
+++ src/controllers/auth/__tests__/login.test.ts
@@ -47,4 +47,20 @@ describe("E2E - Login user", () => {
     expect(res.body.errorRaw).to.an("null");
     expect(res.body.errorsValidation).to.an("null");
   });
+
+  it("should report error when the email provided is not valid", async () => {
+    const res = await request(app)
+      .post("/v1/auth/login")
+      .send({ email: "not_valid_email", password: fixtureUser.password });
+    expect(res.status).to.equal(400);
+    expect(res.body.errorType).to.equal("Validation");
+    expect(res.body.errorMessage).to.equal("Login validation error");
+    expect(res.body.errors).to.an("null");
+    expect(res.body.errorRaw).to.an("null");
+    expect(res.body.errorsValidation).to.eql([
+      {
+        email: "Email is invalid",
+      },
+    ]);
+  });
 });
