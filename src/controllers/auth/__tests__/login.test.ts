9ab67f1fadc43927343a633ec4367d1b2fc5e8f6
luisdev97 <amadormontorodavid@gmail.com>
2023-05-09 21:39:03 +0100

    test: invalid password  use case tested

--- src/controllers/auth/__tests__/login.test.ts
+++ src/controllers/auth/__tests__/login.test.ts
@@ -36,4 +36,15 @@ describe("E2E - Login user", () => {
     expect(res.body.data).not.to.be.empty;
     expect(res.body.data).to.be.an("string");
   });
+
+  it("should report error when password don't match", async () => {
+    const res = await request(app)
+      .post("/v1/auth/login")
+      .send({ email: fixtureUser.email, password: "wrong_password" });
+    expect(res.status).to.equal(401);
+    expect(res.body.errorType).to.equal("General");
+    expect(res.body.errors).to.eql(["Incorrect password"]);
+    expect(res.body.errorRaw).to.an("null");
+    expect(res.body.errorsValidation).to.an("null");
+  });
 });
