3efcdba3b475e1708c18e55795158ebf62698f03
luisdev97 <amadormontorodavid@gmail.com>
2023-05-09 19:57:01 +0100

    refactor: error handling applied to get-client-by-id use case

--- src/controllers/clients/get-client-by-id.ts
+++ src/controllers/clients/get-client-by-id.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { getRepository } from "typeorm";
 
 import { Client as ClientEntity } from "../../orm/entities/Client/Client.entity";
+import { CustomError } from "../../utils/response/custom-error/custom-error";
 
 export const getClientById = async (
   req: Request,
@@ -10,6 +11,20 @@ export const getClientById = async (
 ) => {
   const id = req.params.id;
   const clientRepository = getRepository(ClientEntity);
-  const client = await clientRepository.findOne(id);
-  res.customSuccess(200, "Client found.", client);
+  try {
+    const client = await clientRepository.findOne(id);
+    if (!client) {
+      const customError = new CustomError(
+        404,
+        "General",
+        `Client with id:${id} not found.`,
+        ["Client not found."]
+      );
+      return next(customError);
+    }
+    res.customSuccess(200, "Client found.", client);
+  } catch (err) {
+    const customError = new CustomError(400, "Raw", "Error", null, err);
+    return next(customError);
+  }
 };
