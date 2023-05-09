60637b130ff55533496ab4f63eefb0f0f4712a33
luisdev97 <amadormontorodavid@gmail.com>
2023-05-09 22:20:01 +0100

    feat: get clietn policies route added

--- src/routes/v1/clients.ts
+++ src/routes/v1/clients.ts
@@ -1,20 +1,31 @@
 import { Router } from "express";
 
-import { getClientById, getClientByName } from "../../controllers";
+import {
+  getClientById,
+  getClientByName,
+  getClientPolicies,
+} from "../../controllers";
 import { checkJwt, checkRole } from "../../middleware";
 
 const router = Router();
 const isTestEnvironment = process.env.NODE_ENV == "test";
 
 router.get(
-  "/:id",
-  isTestEnvironment ? [] : [checkJwt, checkRole(["user", "admin"], true)],
-  getClientById
-);
-router.get(
   "/",
   isTestEnvironment ? [] : [checkJwt, checkRole(["user", "admin"], true)],
   getClientByName
+);
+
+router.get(
+  "/policies",
+  isTestEnvironment ? [] : [checkJwt, checkRole(["admin"], true)],
+  getClientPolicies
+);
+
+router.get(
+  "/:id",
+  isTestEnvironment ? [] : [checkJwt, checkRole(["user", "admin"], true)],
+  getClientById
 );
 
 export default router;
